1cd19561f40357695b0cfc7bb73fb7a316b2abca
prishaa23 <priyaprasanthi.pulipati@gmail.com>
2023-05-02 16:07:18 +0530

    first commit

---
+++ src/main/java/com/example/demo/ControllerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerDemoApplication.class, args);
+        System.out.println("hello");
+        System.out.println("displaying name");
+    }
+
+}
